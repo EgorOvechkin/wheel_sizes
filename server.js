0b4c1ab013cb0122ca4c4122fdf038d6edcf2b32
Egor Ovechkin <egorka1391@gmail.com>
2017-09-30 15:47:53 +0500

    use rotes

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const Koa = require('koa');
-var hbs = require('koahub-handlebars');
+const Router = require('koa-router');
+const Boom = require('boom');
+const hbs = require('koahub-handlebars');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(hbs.middleware({
-  extname: '.handlebars',
-  viewPath: './public/templates',
-  layoutsPath: './public/templates',
-  defaultLayout: 'layout'
-}));
+router
+  .get('/', async ctx => {
+    await ctx.render('main', {title: 'test', numbers: [10, 11, 21, 31]});
+  })
 
-app.use(async ctx => {
-  await ctx.render('main', {title: 'test', numbers: [10, 1, 2, 3]})
-});
+app
+  .use(hbs.middleware({
+    extname: '.handlebars',
+    viewPath: './public/templates',
+    layoutsPath: './public/templates',
+    defaultLayout: 'layout'
+  }))
+  .use(router.routes())
+  .use(router.allowedMethods({
+    notImplemented: () => new Boom.notImplemented(),
+    methodNotAllowed: () => new Boom.methodNotAllowed()
+  }))
 
-app.listen(3000);
+  .listen(3000);
