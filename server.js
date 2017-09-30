1395a875dc984a570990a49641a2802380f31ff3
Egor Ovechkin <egorka1391@gmail.com>
2017-09-30 13:31:49 +0500

    use koa and handlebars

---
+++ server.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+var hbs = require('koahub-handlebars');
+const app = new Koa();
+
+app.use(hbs.middleware({
+  extname: '.handlebars',
+  viewPath: './public/templates'
+}));
+
+app.use(async ctx => {
+  await ctx.render('main', {title: 'test', numbers: [10, 1, 2, 3]})
+});
+
+app.listen(3000);
