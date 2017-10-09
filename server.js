0d6c6853039fc5d443aea358808b175fcc3d39a9
semen zhi <401@onlinepbx.ru>
2017-10-09 16:40:29 +0500

    dotenv

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const Koa = require('koa'),
       Router = require('koa-router'),
       Boom = require('boom'),
       hbs = require('koahub-handlebars');
-require('./templates/helpers')
+require('./templates/helpers');
+require('dotenv').config();
+console.log('----- \n', process.env, '\n -------')
 //константы
 const {createMySQLConnection} = require('./helpers'),
       dbOptions = {
-        host: 'localhost',
-        user: 'root',
-        password: '12345',
-        database: 'wheel_sizes'
+        host: process.env.SQL_HOST,
+        user: process.env.SQL_NAME,
+        password: process.env.SQL_KEY,
+        database: process.env.SQLDB_NAME
       },
       db = createMySQLConnection(dbOptions),
       app = new Koa(),
